b2cb775921480ce9ac857099beda67f314c803ec
Sebastian Just Jensen <sebastian_just@hotmail.com>
2023-06-12 15:38:53 +0200

    removed unused import

--- src/middleware/authenticator.tsx
+++ src/middleware/authenticator.tsx
@@ -1,4 +1,3 @@
-import { on } from "events";
 import { getApp } from "firebase/app";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { FC, useEffect } from "react";
@@ -7,22 +6,22 @@ import { useAppContext } from "./context-provider";
 let authInitialized = false;
 
 export const Authenticator: FC = () => {
-	const auth = getAuth(getApp());
-	const dispatch = useAppContext()[1];
+  const auth = getAuth(getApp());
+  const dispatch = useAppContext()[1];
 
-	const listenToAuthState = () => {
-		onAuthStateChanged(auth, (foundUser) => {
-			const user = foundUser ? { ...foundUser } : null;
-			dispatch({ type: "UPDATE_USER", payload: user });
-		});
-	};
+  const listenToAuthState = () => {
+    onAuthStateChanged(auth, (foundUser) => {
+      const user = foundUser ? { ...foundUser } : null;
+      dispatch({ type: "UPDATE_USER", payload: user });
+    });
+  };
 
-	useEffect(() => {
-		if (!authInitialized) {
-			listenToAuthState();
-			authInitialized = true;
-		}
-	}, []);
+  useEffect(() => {
+    if (!authInitialized) {
+      listenToAuthState();
+      authInitialized = true;
+    }
+  }, []);
 
-	return <></>;
+  return <></>;
 };
